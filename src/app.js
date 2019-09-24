1dd074b982f3dff78836f7554a7491915c02aee0
Chris <christophertberg@gmail.com>
2019-09-23 19:17:26 -0700

    Git init.  Building first REACT project, which is an Indecision App that takes random tasks as inputs and tells you what to do when a button is clicked.

---
+++ src/app.js
@@ -0,0 +1,101 @@
+class IndecisionApp extends React.Component {
+    render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer.';
+        const options = ['Thing one', 'Thing two', 'Thing four'];
+
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <Action />
+                <Options options={options} />
+                <AddOption />
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
+            </div>
+        );
+    }
+}
+
+class Action extends React.Component {
+    handlePick() {
+        alert('handlePick');
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handlePick}>What should I do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        // Resetting handleRemoveAll's value makes sure wherever we call it the context is correct.
+        // The context is correct because much like render(), constructor() is not an event callback, so that's why we can call this inside our call to bind.
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
+
+    handleRemoveAll() {
+        console.log(this.props.options);
+        alert('removeAll');
+    } 
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleRemoveAll}>Remove All Options.</button>
+                {
+                    this.props.options.map((option) => <Option key={option} optionText={option} />)
+                }
+            </div>
+        );
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.optionText}
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault();
+
+        const option = e.target.elements.option.value.trim();
+
+        if(option) {
+            alert(option);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleAddOption}>
+                    <input placeholder="Type your option here." type="text" name="option"></input>
+                    <button>Add Option.</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
