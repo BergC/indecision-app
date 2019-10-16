6cf8623ff2661b9a146fca3af229da7efe9ac9df
Chris <christophertberg@gmail.com>
2019-10-15 22:18:04 -0700

    Began styling the app using SCSS.  Styles were set using BEM methodology.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
+// Normalize.css import for CSS reset to all browsers.
+import 'normalize.css/normalize.css';
+
 // Import our styles file.
 import './styles/styles.scss';
 
--- src/components/Options.js
+++ src/components/Options.js
@@ -6,8 +6,16 @@ import Option from './Option';
 // We access handleDeleteOptins by accessing this.props.
 const Options = (props) => (
     <div>
-        <button onClick={props.handleDeleteOptions}>Remove All Options.</button>
-        {props.options.length === 0 && <p>Please add an option to get started!</p>}
+        <div className="widget-header">       
+            <h3 className="widget-header__title">Your Options</h3>
+            <button
+                className="button button--link" 
+                onClick={props.handleDeleteOptions}
+            >
+                Remove All
+            </button>
+        </div>
+        {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
         {
             props.options.map((option) => (
                 <Option
