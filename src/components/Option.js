488eb1733777f092e509514f2b52d84fd1107726
Chris <christophertberg@gmail.com>
2019-10-16 22:56:01 -0700

    Continued working on styling the application using SCSS.

--- src/components/Option.js
+++ src/components/Option.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 // Maintain Option variable so that React Dev Tools displays valid name. Export below to maintain.
 const Option = (props) => (
-    <div>
-        {props.optionText}
+    <div className="option">
+        <p className="option__text">{props.count}. {props.optionText}</p>    
         <button
             className="button button--link"
             onClick={(e) => {
